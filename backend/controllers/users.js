f9b44c4bd76f97f0fd7ca7624eccc722869d7aa0
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-10-19 17:15:40 +0300

    Debug checking if there's already a friend request

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -70,6 +70,10 @@ usersRouter.post('/friendRequest', async (req, res) => {
   // Check that there isn't already a friendrequest.
   const existingRequest = senderUser.friendRequests.find(r => r.sender == receiverUser._id)
   const existingSentRequest = senderUser.sentFriendRequests.find(r => r.receiver == receiverUser._id)
+  console.log("existing request:")
+  console.log(existingRequest._id)
+  console.log("existing sent request:")
+  console.log(existingSentRequest._id)
   if (existingRequest || existingSentRequest){
     res.status(409).end()
     return
