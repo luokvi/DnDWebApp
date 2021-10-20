64f93db8f80b15da4bce450e3bca6ea85aee641e
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-10-20 17:46:15 +0300

    Add error handling for tokenExpiredError

--- backend/util/middleware.js
+++ backend/util/middleware.js
@@ -2,6 +2,10 @@ const errorHandler = (error, req, res, next) => {
     if (error.name === 'JsonWebTokenError'){
         res.status(401).json({ error: 'Invalid Token' })
         return
+    } 
+    else if (error.name === 'TokenExpiredError'){
+        res.status(401).json({ error: 'Token Expired' })
+        return
     }
 
     next(error)
