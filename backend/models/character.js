caf23430d85dbb3248517e72b94d10f4629c96c5
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-10-21 14:42:00 +0300

    Define Character schema

---
+++ backend/models/character.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then( () => {
+    console.log('connected!')
+  })
+  .catch((error) => {
+    console.log('error connecting: ', error.message)
+  })
+
+const characterSchema = new mongoose.Schema({
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String },
+    race: { type: String },
+    class: { type: String },
+    level: { type: Number, min: 0 },
+    expreriencePoints: { type: Number, min: 0 },
+    background: { type: String },
+    personalityTraits: { type: String },
+    alignment: { 
+        type: String,
+        enum: ['true neutral', 'neutral evil', 'neutral good', 'chaotic neutral', 'chaotic evil', 'chaotic good', 'lawful neutral', 'lawful evil', 'lawful good']
+        },
+    maxHealth: { type: Number, min: 0 },
+    currentHealth: { type: Number, min: 0 },
+    hitDice: { type: String },
+    armorClass: { type: Number, min: 0 },
+    initiative: { type: Number, min: -20 },
+    speed: { type: Number, min: 0 },
+    strength: { type: Number, min: 0, max: 20 },
+    dexterity: { type: Number, min: 0, max: 20 },
+    constitution: { type: Number, min: 0, max: 20 },
+    intelligence: { type: Number, min: 0, max: 20 },
+    wisdom: { type: Number, min: 0, max: 20 },
+    charisma: { type: Number, min: 0, max: 20 },
+    passiveWisdom: { type: Number, min: 0 },
+    proficiences: [{ type: String }],
+    languages: [{ type: String }],
+    equipment: { type: String },
+    features: [{ type: Map, of: String }],
+    coins: { 
+        copper: { type: Number, min: 0 },
+        silver: { type: Number, min: 0 },
+        gold: { type: Number, min: 0 },
+        platinum: { type: Number, min: 0 },
+        ep: { type: Number, min: 0 }
+        }
+    
+})
+
+characterSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+module.exports = mongoose.model("Character", characterSchema)
