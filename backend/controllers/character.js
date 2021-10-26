c6395e4818a5953a477fe3e7bd5f0e403e4d36f8
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-10-26 11:22:42 +0300

    Add module export

--- backend/controllers/character.js
+++ backend/controllers/character.js
@@ -47,6 +47,9 @@ charaRouter.post('/', async (req, res) => {
         proficiencies: body.proficiencies,
         languages: body.languages,
         otherProficiencies: body.otherProficiencies,
+        weapons: body.weapons,
+        spellCastingAbility: body.spellCasting,
+        spells: body.spells,
         equipment: body.equipment,
         storage: body.storage,
         features: body.features,
@@ -89,3 +92,5 @@ charaRouter.delete('/:id', async (req, res) => {
 
     res.status(204).end()
 })
+
+module.exports = charaRouter
