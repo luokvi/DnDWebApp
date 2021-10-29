cac4636a69f93fe45c5b121eca5e8c9dc8ff1797
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-10-29 15:35:19 +0300

    Add field creator

--- backend/models/encounter.js
+++ backend/models/encounter.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const encounterSchema = new mongoose.Schema({
+	creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	name: { type: String, required: true },
 	enemies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Enemy' }],
 	npcs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Character' }],
