d89d6e5a47e8fab1afd2cde6b551a75ca2ac7bb1
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-10-29 11:42:47 +0300

    Create Encounter schema

---
+++ backend/models/encounter.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const encounterSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	enemies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Character' }],
+	npcs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Character' }],
+	loot: {
+		weapons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Weapon' }],
+		equipments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Equipment' }],
+		coins: {
+			copper: { type: Number, min: 0 },
+			silver: { type: Number, min: 0 },
+			gold: { type: Number, min: 0 },
+			electrum: { type: Number, min: 0 },
+			platinum: { type: Number, min: 0 }
+		}
+	},
+	experience: { type: Number, min: 0},
+	notes: [{ type: String }]
+})
+
+encounterSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+module.exports = mongoose.model("Encounter", encounterSchema)
