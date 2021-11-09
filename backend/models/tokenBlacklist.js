8d5a80138611f131da79eadc20b0ac1392c49021
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-11-09 13:29:03 +0200

    Update tokenBlacklist model

--- backend/models/tokenBlacklist.js
+++ backend/models/tokenBlacklist.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
 const blacklistSchema = new mongoose.Schema({
     token: { type: String, required: true },
     time: { type: Date, required: true }
