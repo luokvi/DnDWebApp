9f4e54516ebccfa5c7118823e841d2c68447a2b4
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-11-09 18:41:44 +0200

    Create login service

---
+++ frontend/src/services/login.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const baseUrl = 'api/login';
+
+const loginUser = async (username, password) => {
+    const user = {
+        username: username,
+        password: password
+    }
+    const res = await axios.post(baseUrl, user)
+
+    return res.data
+}
+
+export default { getAll }
