816012baad6af0da68d58b3823d1de094b88cdc5
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-11-13 18:57:28 +0200

    Update myProfilePage.js

--- frontend/src/components/myProfilePage.js
+++ frontend/src/components/myProfilePage.js
@@ -1,29 +1,26 @@
 import React, { useState } from "react"
-import userService from '../services/users'
 
-const ProfilePage = ({ userid }) => {
-    const [user, setUser] = useState('')
-    if (userid === ''){
+const ProfilePage = ({ user }) => {
+    try {
+        console.log("userid: " + user.id)
+        console.log("user: " + user )
+
+        return(
+            <div>
+                <h2> Characters: </h2>
+                {user.characters.map(chara =>
+                    <p key={chara.id}>{chara.name}</p>
+                )}
+            </div>
+        )
+    } catch {
         return(
             <div>
                 <p>Not logged in.</p>
             </div>
         )
     }
-    console.log("userid: " + userid)
-    userService.getUser(userid).then( profile => {
-        setUser(profile)
-    })
-    console.log("user: " + user )
-
-    return(
-        <div>
-            <h2> Characters: </h2>
-            {user.characters.map(chara =>
-                <p key={chara.id}>{chara.name}</p>
-            )}
-        </div>
-    )
+    
 
 }
 
