161156601d8950226dfc13123a2c021680d76c81
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-11-14 17:47:15 +0200

    User's chararacters on profile page

--- frontend/src/components/myProfilePage.js
+++ frontend/src/components/myProfilePage.js
@@ -1,11 +1,28 @@
-import React, { useState } from "react"
+import React from "react"
 
+const Character = ({ c }) => {
+    return(
+        <div>
+            <h4>{c.name}</h4>
+            <p>{c.level} level {c.class}, {c.race}</p>
+        </div>
+    )
+}
 const ProfilePage = ({ user }) => {
-    console.log(JSON.stringify(user))
+    
+    if (user.characters === undefined){
+        return(
+            <div>empty</div>
+        )
+    }
+
     return(
         <div>
             <h2>{user.username}</h2>
             <h3>Characters:</h3>
+            {user.characters.map(chara => 
+                <Character c={chara} />
+                )}
         </div>
     )
 }
