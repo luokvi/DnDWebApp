6c0602240b0356fdcf5cb25f77b82ea9bf8ec130
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-11-14 17:32:18 +0200

    Debug myProfilePage

--- frontend/src/components/myProfilePage.js
+++ frontend/src/components/myProfilePage.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
 
 const ProfilePage = ({ user }) => {
-    
+    console.log(JSON.stringify(user))
     return(
         <div>
             <h2>{user.username}</h2>
+            <h3>Characters:</h3>
         </div>
     )
 }
