f3a0708d576c330c3cd02059602395e3bcf6ef1a
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-12-29 17:31:51 +0200

    Use tuple

--- frontend/src/components/formComponents.js
+++ frontend/src/components/formComponents.js
@@ -85,7 +85,7 @@ export const AddToSimpleList = ({ field, listValue, listSetFunction }) => {
 }
 
 export const DropDownList = ({ field, optionsList, listValue, listSetFunction }) => {
-    const [selected, setSelected] = useState("")
+    const [selected, setSelected] = useState(("", ""))
     const [addedItemNames, setItemNames] = useState([])
 
     const add = (event) => {
@@ -93,10 +93,10 @@ export const DropDownList = ({ field, optionsList, listValue, listSetFunction })
         if (selected === ""){
             return
         }
-
-        console.log("selected: " + selected.name)
-        listSetFunction([...listValue, selected.id])
-        setItemNames([...addedItemNames, selected.name])
+        const [id, name] = selected
+        console.log("selected: " + id + ", " + name)
+        listSetFunction([...listValue, id])
+        setItemNames([...addedItemNames, name])
 
         setSelected("")
     }
@@ -106,7 +106,7 @@ export const DropDownList = ({ field, optionsList, listValue, listSetFunction })
             <h5>{field}</h5>
             <select name={field} id={field} onChange={({ target }) => setSelected(target.value)}>
                 {optionsList.map(optionValue =>
-                    <option key={optionValue.id} value={{id: optionValue.id, name: optionValue.name}}>{optionValue.name}</option>
+                    <option key={optionValue.id} value={(optionValue.id, optionValue.name)}>{optionValue.name}</option>
                 )}
                 <option>empty</option>
             </select>
