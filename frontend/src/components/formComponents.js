0b4d31dec73efa5c7751445acbb29dada2816eb0
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-02 19:11:08 +0200

    Add checkboxes for spell components

--- frontend/src/components/formComponents.js
+++ frontend/src/components/formComponents.js
@@ -90,6 +90,9 @@ export const DropDownList = ({ field, optionsList, listValue, listSetFunction, n
     const [spellLevel, levelSet] = useState("")
     const [castingTime, castingTimeSet] = useState("")
     const [rangeValue, rangeSet] = useState("")
+    const [verbal, verbalSet] = useState("")
+    const [somatic, somaticSet] = useState("")
+    const [material, materialSet] = useState("")
     const [componentsValue, componentsSet] = useState([""])
     const [minutesValue, minutesSet] = useState("")
     const [isConcentrationValue, concentrationSet] = useState("")
@@ -117,11 +120,25 @@ export const DropDownList = ({ field, optionsList, listValue, listSetFunction, n
     const createNew = async (event) => {
         event.preventDefault()
 
+        componentsSet([verbal, somatic, material])
+        const isConcentration = isConcentrationValue === "Is Concentration" ? true : false
+
         // Add to backend.
         const newItem = {
             "itemType": itemType,
             "name": nameValue,
-            "description": descriptionValue
+            "description": descriptionValue,
+            "level": spellLevel,
+            "castingTime": castingTime,
+            "range": rangeValue,
+            "components": componentsValue,
+            "duration": {
+                "minutes": minutesValue,
+                "isConcentration": isConcentration
+            },
+            "atkBonus": atkValue,
+            "damage": damageValue
+            
         }
 
         const createdItem = await newItemFunction(newItem)
@@ -144,15 +161,15 @@ export const DropDownList = ({ field, optionsList, listValue, listSetFunction, n
             case "Equipment":
                 return(
                     <div>
-                        <FormField id="itemName" title="Name" value={nameValue} setFunction={nameSet} />
-                        <FormField id="itemDesc" title="Description" value={descriptionValue} setFunction={descriptionSet}/>
+                        <FormField id="eName" title="Name" value={nameValue} setFunction={nameSet} />
+                        <FormField id="eDesc" title="Description" value={descriptionValue} setFunction={descriptionSet}/>
                     </div>
                 )
             case "Weapon":
                 return(
                     <div>
-                        <FormField id="itemName" title="Name" value={nameValue} setFunction={nameSet} />
-                        <FormField id="itemDesc" title="Description" value={descriptionValue} setFunction={descriptionSet}/>
+                        <FormField id="wName" title="Name" value={nameValue} setFunction={nameSet} />
+                        <FormField id="wDesc" title="Description" value={descriptionValue} setFunction={descriptionSet}/>
                         <FormField id="weaponAtk" title="ATK Bonus" value={atkValue} setFunction={atkSet} type="Number" />
                         <FormField id="weaponDmg" title="Damage Type" value={damageValue} setFunction={damageSet} />
                         <FormField id="weaponRange" title="Range" value={rangeValue} setFunction={rangeSet} type="Number" />
@@ -161,14 +178,21 @@ export const DropDownList = ({ field, optionsList, listValue, listSetFunction, n
             case "Spell":
                 return(
                     <div>
-                        <FormField id="itemName" title="Name" value={nameValue} setFunction={nameSet} />
-                        <FormField id="itemDesc" title="Description" value={descriptionValue} setFunction={descriptionSet}/>
+                        <FormField id="sName" title="Name" value={nameValue} setFunction={nameSet} />
+                        <FormField id="sDesc" title="Description" value={descriptionValue} setFunction={descriptionSet}/>
                         <FormField id="spellLevel" title="Level" value={spellLevel} setFunction={levelSet} type="Number" />
                         <FormField id="spellCasting" title="Casting Time" value={castingTime} setFunction={castingTimeSet} />
                         <div>
+                            <p>Components needed:</p>
+                            <CheckboxField title="Verbal" setFunction={verbalSet} />
+                            <CheckboxField title="Somatic" setFunction={somaticSet} />
+                            <CheckboxField title="Material" setFunction={materialSet} />
+                        </div>
+                        <FormField id="spellRange" title="Range" value={rangeValue} setFunction={rangeSet} type="Number" />                        
+                        <div>
                             <p>Spell Duration</p>
                             <FormField id="spellMinutes" title="Duration, minutes" value={minutesValue} setFunction={minutesSet} type="Number" />
-                            <CheckboxField title={"Is Concentration"} setFunction={concentrationSet}/>
+                            <CheckboxField title="Is Concentration" setFunction={concentrationSet}/>
                         </div>
                     </div>
                 )
