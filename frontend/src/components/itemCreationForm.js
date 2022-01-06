010cbd14e67bee22a4332f205664d5447eb7a317
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-06 11:46:05 +0200

    Redo equipment creation

--- frontend/src/components/itemCreationForm.js
+++ frontend/src/components/itemCreationForm.js
@@ -6,24 +6,47 @@ import { CheckboxField, FormField } from './formComponents'
 
 // TODO: separate function for adding different types of items??
 const ItemCreationForm = ({ userId, token }) => {
-    const [itemNameValue, itemNameSet] = useState("")
-    const [itemDescriptionValue, itemDescriptionSet] = useState("")
+    const [equipNameValue, equipNameSet] = useState("")
+    const [equipDescriptionValue, equipDescriptionSet] = useState("")
+
+    const [spellNameValue, spellNameSet] = useState("")
+    const [spellDescriptionValue, spellDescriptionSet] = useState("")
     const [spellLevel, levelSet] = useState("")
     const [castingTime, castingTimeSet] = useState("")
-    const [rangeValue, rangeSet] = useState("")
+    const [spellRangeValue, spellRangeSet] = useState("")
     const [verbal, verbalSet] = useState("")
     const [somatic, somaticSet] = useState("")
     const [material, materialSet] = useState("")
     const [components, componentsSet] = useState([])
     const [minutesValue, minutesSet] = useState("")
     const [isConcentrationValue, concentrationSet] = useState("")
+
+    const [itemNameValue, itemNameSet] = useState("")
+    const [itemDescriptionValue, itemDescriptionSet] = useState("")
     const [atkValue, atkSet] = useState("")
     const [damageValue, damageSet] = useState("")
+    const [weaponRangeValue, weaponRangeSet] = useState("")
+
+    const createEquip = async () => {
+        const newEquip = {
+            "name": equipNameValue,
+            "description": equipDescriptionValue,
+            "itemType": "Equipment",
+            "userId": userId
+        }
+        const created = await createService.createItem(newEquip, token)
+
+        console.log(created)
+    }
 
     return(
-        <di>
-            
-        </di>
+        <div>
+            <form onSubmit={createEquip}>
+                <FormField id="EquipmentName" title="Name" type="text" value={equipNameValue} setFunction={equipNameSet} />
+                <FormField id="EquipmentDesc" title="Description" type="text" value={equipDescriptionValue} setFunction={equipDescriptionSet} />
+                <button type="Submit">Create</button>
+            </form>
+        </div>
     )
 }
 
