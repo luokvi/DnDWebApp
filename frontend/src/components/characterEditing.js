41c4d0862aa674af9d1126678a067690078c445d
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-13 16:50:43 +0200

    Edit function

--- frontend/src/components/characterEditing.js
+++ frontend/src/components/characterEditing.js
@@ -1,4 +1,4 @@
-import react, { useState  } from "react"
+import react, { useEffect, useState  } from "react"
 import { useParams } from "react-router-dom"
 
 import createService from '../services/creations'
@@ -7,8 +7,19 @@ const EditingCharacter = ({ token }) => {
     const [characterToEdit, setCharacter] = useState("")
     const { characterId } = useParams()
 
+    useEffect(() => {
+        GetCharacterToEdit()
+    }, [])
+
     const GetCharacterToEdit = async () => {
-        
+        if (characterId === undefined){
+            return(
+                <div>
+                    <p>...</p>
+                </div>
+            )
+        }
+
         const chara = await createService.getCharacter(characterId, token)
 
         console.log("Chara:")
@@ -18,20 +29,10 @@ const EditingCharacter = ({ token }) => {
           
     }
 
-    if (characterId === undefined){
-        return(
-            <div>
-                <p>...</p>
-            </div>
-        )
-    } else {
-        GetCharacterToEdit()
-    }
-
     return(
-        <di>
+        <div>
             <p>Editing character {characterId}</p>
-        </di>
+        </div>
     )
 }
 
