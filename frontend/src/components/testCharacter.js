339d131b484d9051c141094c7bd507537efaa0ed
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-14 16:37:35 +0200

    Test setting a name

--- frontend/src/components/testCharacter.js
+++ frontend/src/components/testCharacter.js
@@ -1,8 +1,48 @@
-import react from "react"
+import react, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
-const NewForm = () => {
+import createService from '../services/creations'
 
+const NewForm = ({ token }) => {
+    const { characterId } = useParams()
+    const [character, setCharacter] = useState({})
+    const [name, setName] = useState("")
+
+    useEffect(() => {
+        getCharacter()
+    }, [])
+
+    const getCharacter = async () => {
+        if (characterId === undefined){
+            return
+        }
+        
+        const chara = await createService.getCharacter(characterId, token)
+        setCharacter(chara)
+        setName(chara.name)
+
+        console.log("Chara:")
+        console.log(chara)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        console.log("Submit")
+        console.log(character)
+        console.log(name)
+    }
+
+    return(
+        <div>
+            <h2>Testing...</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label>
+                <input type="text" value={name} name={"Name"} onChange={({ target }) => setName(target.value)}/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default NewForm
