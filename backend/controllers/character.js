e2dc09897d6cca21ed1fd87efd6e8221600e2c71
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-16 13:22:42 +0200

    Fix setting spellCastingAbility

--- backend/controllers/character.js
+++ backend/controllers/character.js
@@ -49,7 +49,7 @@ charaRouter.post('/', async (req, res) => {
         languages: body.languages,
         otherProficiencies: body.otherProficiencies,
         weapons: body.weapons,
-        spellCastingAbility: body.spellCasting,
+        spellCastingAbility: body.spellCastingAbility,
         spells: body.spells,
         equipment: body.equipment,
         storage: body.storage,
