e5a86b815a0465fa31cd0eedfc13eaa577490236
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-20 18:44:43 +0200

    Check if user is friend

--- frontend/src/components/userPage.js
+++ frontend/src/components/userPage.js
@@ -8,6 +8,7 @@ const UserPage = ({ user, token }) => {
     const { id } = useParams()
 
     const [gotUser, setUser] = useState("")
+    const [isFriend, setIsFriend] = useState(false)
 
     useEffect(() => {
         getUser()
@@ -16,11 +17,19 @@ const UserPage = ({ user, token }) => {
     const getUser = async () => {
         const u = await userService.getOtherUser(id)
         setUser(u)
+
+        // Check if this user is a friend.
+        const userIsFriend = user.friends.includes(id)
+        console.log("User is friend: " + userIsFriend)
+        setIsFriend(userIsFriend)
     }
 
     return(
         <div>
             <h2>User {gotUser.username}</h2>
+            {isFriend ? <p>A friend!</p>
+                : <button>Send friendrequest</button>
+            }
         </div>
     )
 }
