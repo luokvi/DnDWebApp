ff994b6d1508321c084fc3e347c00e5c25d315fd
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-27 15:55:34 +0200

    Character page route

--- frontend/src/components/characterPage.js
+++ frontend/src/components/characterPage.js
@@ -1,9 +1,37 @@
 import React, { useState, useEffect } from "react"
 import { useParams, Link } from "react-router-dom"
 
-const CharacterPage = ({ }) => {
+import creationsService from '../services/creations'
+
+const CharacterPage = ({ token }) => {
     const id = useParams
     const [chara, setChara] = useState("")
+
+    useEffect(() => {
+        getCharacter()
+    },[])
+
+    const getCharacter = async () => {
+        const response = creationsService.getCharacter(id, token)
+        setChara(response)
+    }
+
+    if (chara === ""){
+        return(
+            <div>
+                <p>loading...</p>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <h2>{chara.name}</h2>
+            <Link to={'/user/' + chara.creator.id}>
+                {chara.creator.username}
+            </Link>
+        </div>
+    )
 }
 
 export default CharacterPage
