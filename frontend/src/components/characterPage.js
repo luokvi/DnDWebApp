b555cde4aab1603525a4ff2c64f6b3492e8a849a
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-27 15:47:04 +0200

    Create page for character

---
+++ frontend/src/components/characterPage.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+
+const CharacterPage = ({ }) => {
+    const id = useParams
+    const [chara, setChara] = useState("")
+}
+
+export default CharacterPage
