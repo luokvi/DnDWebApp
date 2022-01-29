38890aacae59c33b9cf56d570ff6f1d9dd76f339
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-29 20:22:08 +0200

    Move files to project root

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+require('express-async-errors')
+app.use(express.json())
+
+const userRouter = require('./backend/controllers/users')
+app.use('/api/users', userRouter)
+
+const loginRouter = require('./backend/controllers/login')
+app.use('/api/login', loginRouter)
+
+const characterRouter = require('./backend/controllers/character')
+app.use('/api/character', characterRouter)
+
+const itemsRouter = require('./backend/controllers/items')
+app.use('/api/items', itemsRouter)
+
+const campaignRouter = require('./backend/controllers/campaign')
+app.use('/api/dm', campaignRouter)
+
+const middleware = require('./backend/util/middleware')
+app.use(middleware.errorHandler)
+
+app.get('/', ( req, res ) => {
+  res.send( '<h1>Hello World!</h1>' )
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
