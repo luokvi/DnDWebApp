21ffb637a41e3f905a248c035fb3067c89725447
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2022-01-29 18:41:59 +0200

    Fix typo

--- backend/controllers/character.js
+++ backend/controllers/character.js
@@ -58,7 +58,7 @@ charaRouter.post('/', async (req, res) => {
         charisma: body.charisma,
         passiveWisdom: body.passiveWisdom,
         proficiencyBonus: body.proficiencyBonus,
-        proficientSkills: body.proficiencientSkills,
+        proficientSkills: body.proficientSkills,
         languages: body.languages,
         otherProficiencies: body.otherProficiencies,
         weapons: body.weapons,
